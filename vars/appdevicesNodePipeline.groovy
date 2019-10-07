37fd4f215ced850e22e093de16a503cab4fed7b7
Chris Reid <chris.reid@appdirect.com>
2019-10-07 13:44:24 -0600

    adjusting pipeline

--- vars/appdevicesNodePipeline.groovy
+++ vars/appdevicesNodePipeline.groovy
@@ -1,32 +1,144 @@
 // vars/appdevicesNodePipeline.groovy
+
+def withDockerCompose(Closure body) {
+  docker.image('docker/compose:1.24.1').inside("-v /var/run/docker.sock:/var/run/docker.sock --entrypoint=''", body)
+}
+
+def withSonarScanner(Closure body) {
+  docker.image('newtmitch/sonar-scanner:alpine').inside("--entrypoint=''", body)
+}
+
 def call(Map pipelineParams) {
-    pipeline {
-        agent any
-        stages {
-            stage('checkout git') {
-                steps {
-                    echo "Just saying hello world:" + pipelineParams.name
-                }
-            }
-
-            stage('build') {
-                steps {
-                    echo "Just saying hello world, but this time in a build"
-                }
-            }
-
-            stage ('test') {
-                steps {
-                    echo "Just saying hello world, but this time running tests"
-                }
-            }
-        }
-/*
-        post {
-            failure {
-                mail to: pipelineParams.email, subject: 'Pipeline failed', body: "${env.BUILD_URL}"
-            }
-        }
-*/
+  pipeline {
+    agent any
+    options { disableConcurrentBuilds() }
+    stage("Checkout") {
+      steps {
+        checkoutWithEnv()
+      }
     }
+
+    stage("Semantic Version") {
+      steps {
+        script {
+          version = getSemver("master")
+          echo version
+          echo "Just saying hello world:" + pipelineParams.name
+        }
+      }
+    }
+
+    /*stage("Skip CI?") {
+      steps {
+        script {
+          def tagForHead = sh(script: "git tag -l --points-at HEAD", returnStdout: true).trim()
+
+          if (tagForHead == version) {
+            echo "This commit is already tagged with version ${tagForHead}. Aborting."
+            currentBuild.result = "ABORTED"
+            error "Skip CI"
+          }
+        }
+      }
+    }
+
+    stage("Build") {
+      steps {
+        withDockerCompose {
+          sh "docker-compose -p ${env.BUILD_TAG} build"
+        }
+      }
+    }
+
+    stage("Unit") {
+      steps {
+        withDockerCompose {
+          sh "docker-compose -p ${env.BUILD_TAG} run --rm ai-appinsights-sgw &quot;test&quot;"
+        }
+      }
+    }
+
+    stage("SonarQube") {
+      steps {
+        withCredentials([string(credentialsId: 'sonar-token', variable: 'SONARQUBE_TOKEN')]) {
+          withSonarScanner {
+            sh "sonar-scanner -Dsonar.login=${env.SONARQUBE_TOKEN} -Dsonar.projectBaseDir=${env.WORKSPACE} -Dsonar.projectVersion=${version}"
+          }
+        }
+      }
+    }
+
+    stage("Audit") {
+      steps {
+        withDockerCompose {
+          sh "docker-compose -p ${env.BUILD_TAG} run --rm ai-appinsights-sgw audit"
+        }
+      }
+    }
+
+    stage("Smoke") {
+      steps {
+        withDockerCompose {
+          sh "docker-compose -p ${env.BUILD_TAG} run --rm ai-appinsights-sgw-smoke"
+        }
+      }
+    }
+
+    stage("Publish") {
+      steps {
+        script {
+          docker.withRegistry("https://docker.appdirect.tools", "docker-rw") {
+            docker.image("docker.appdirect.tools/ai-appinsights-sgw/ai-appinsights-sgw").push(version)
+            docker.image("docker.appdirect.tools/ai-appinsights-sgw/ai-appinsights-sgw-smoke").push(version)
+          }
+        }
+      }
+    }
+
+    stage("Increment Version") {
+      steps {
+        sh "npm version ${version} --no-git-tag-version"
+      }
+    }
+
+    stage("Save NPM Credentials") {
+      steps {
+        withCredentials([[
+              $class          : "UsernamePasswordMultiBinding",
+              credentialsId   : "artifactory-username-password",
+              usernameVariable: "ARTIFACTORY_USER",
+              passwordVariable: "ARTIFACTORY_PASSWD"
+            ]]) {
+          sh "curl -u${env.ARTIFACTORY_USER}:${env.ARTIFACTORY_PASSWD} 'https://artifactory.appdirect.tools/artifactory/api/npm/npm-local/auth/appdirect' >> .npmrc"
+        }
+      }
+    }
+
+    stage("Publish NPM") {
+      steps {
+        sh "npm publish --scope=@appdirect"
+      }
+    }
+
+    stage("Push Git Tags") {
+      when { branch "master-ad-migrate" }
+      steps {
+        sshagent(credentials: ["jenkins-github"]) {
+          sh "git tag ${version}"
+          sh "git push origin --tags"
+        }
+      }
+    }*/
+  }
+  post {
+    always {
+      withDockerCompose {
+        sh "docker-compose -p ${env.BUILD_TAG} down --volumes --remove-orphans"
+        sh "docker-compose rm --force"
+      }
+
+      slackBuildStatus slackChannel, env.SLACK_USER
+    }
+  }
+}
 }
